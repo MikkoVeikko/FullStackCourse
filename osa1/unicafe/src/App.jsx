3f1df92e65a76ab886dcdbd4a548e956a2cb08a8
MikkoVeikko <165706048+MikkoVeikko@users.noreply.github.com>
2024-06-08 16:55:18 +0300

    remade / simplified calculateAverage()

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -41,83 +41,58 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
     const [all, setAll] = useState(0)
-    const data = {
-        good: good,
-        neutral: neutral,
-        bad: bad
-    }
+    const [average, setAverage] = useState([])
 
     const calculatePositive = () => {
         const positive = all === 0 ? 0 : (good / all) * 100
-        return positive === 0 ? 0 :  positive.toFixed(1) + '%'
+        return positive === 0 ? 0 : positive.toFixed(1) + '%' // conditions to prevent return NaN
     }
 
-    const calculateAverage = (data) => {
-        const points = {
-            good: 1,
-            neutral: 0,
-            bad: -1
+    const calculateAverage = () => {
+        let result = 0
+        for (let i = 0; i < average.length; i ++) {
+            result += average[i]
         }
-        let total = 0
-        let count = 0
-
-        for (let key in points) {
-            if (data[key]) {
-                total += data[key] * points[key]
-                count += data[key]
-            }
-        }
-        return count === 0 ? 0 : (total / count).toFixed(1)
-}
+        return result === 0 ? 0 : (result / average.length).toFixed(1)
+    }
 
     const increaseGood = () => {
         const updatedGood = good + 1
         setGood(updatedGood)
         setAll(updatedGood + neutral + bad)
-        calculateAverage(data)
         calculatePositive()
+        setAverage(average.concat(1))
     }
 
     const increaseNeutral = () => {
         const updatedNeutral = neutral + 1
         setNeutral(updatedNeutral)
         setAll(updatedNeutral + good + bad)
-        calculateAverage(data)
         calculatePositive()
+        setAverage(average.concat(0))
     }
 
     const increaseBad = () => {
         const updatedBad = bad + 1
         setBad(updatedBad)
         setAll(updatedBad + good + neutral)
-        calculateAverage(data)
         calculatePositive()
+        setAverage(average.concat(-1))
     }
-
-
 
     return (
         <div>
             <h2>Give feedback</h2>
-            <Button
-                handler={increaseGood}
-                name='good'
-                />
-            <Button
-                handler={increaseNeutral}
-                name='neutral'
-                />
-            <Button
-                handler={increaseBad}
-                name='bad'
-                />
+            <Button handler={increaseGood} name='good'/>
+            <Button handler={increaseNeutral} name='neutral'/>
+            <Button handler={increaseBad} name='bad'/>
             <h2>statistics</h2>
             <Statistics
                 good={good}
                 neutral={neutral}
                 bad={bad}
                 all={all}
-                average={calculateAverage(data)}
+                average={calculateAverage()}
                 positive={calculatePositive()}
             />
         </div>
